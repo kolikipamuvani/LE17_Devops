5ea4304785ae6f14de7e4a483474843043fa0bf7
kolik <kolik@LAPTOP-3LIL981S>
2025-09-03 15:24:17 +0530

    new commit

---
+++ LE17_DevOps/src/Lbrce_DevOps/HelloWorld.java
@@ -0,0 +1,10 @@
+                                                                               package Lbrce_DevOps;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
